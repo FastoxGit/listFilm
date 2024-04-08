61cd9f1bd920b3dba5fbd064c48472a70444ee2e
FastoxGit <romain.joseph77@gmail.com>
2024-04-08 14:57:38 +0200

    Modif design + getDate

--- src/js/pages/home.tsx
+++ src/js/pages/home.tsx
@@ -3,13 +3,15 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 // AXIOS
 import Axios from "axios";
+// MOMENT
+import moment from 'moment';
+
 // TOKEN
 import token from "../token";
 // MUI
-import { Paper } from '@mui/material';
 
 function Home() {
-
+    moment.locale("fr");
     const [data, setData] = useState({});
     const navigate = useNavigate();
 
@@ -29,11 +31,11 @@ function Home() {
         }
     }
 
-    const handleOpenMovieUnique = (id) => {
-        console.log("je clique sur " + id);
+    const handleOpenMovieUnique = (id, overview) => {
         navigate('/movie/unique', {
             state: {
-                id: id
+                id: id,
+                overview: overview
             }
         });
     }
@@ -48,20 +50,24 @@ function Home() {
         <div className='row mx-0 justify-content-center'>
                 {Object.keys(data).length > 0 && (
                     data.results.map((movie, i) => 
-                        <Paper key={i} className='col-3 bg-white m-2 justify-content-center text-center' style={{ height: "15rem", width: "15rem" }}>
+                        <div key={i} className='col-2 m-2 justify-content-center' style={{ border: "1px solid black", borderRadius: "10px" }}>
                             {movie.poster_path !== null ? (
                                 <img 
-                                    className='mt-3' 
-                                    style={{ height: "70%", cursor: "pointer" }} 
+                                    className='mt-3 d-block m-auto rounded' 
+                                    style={{ height: "60%", cursor: "pointer" }} 
                                     src={'https://media.themoviedb.org/t/p/w220_and_h330_face' + movie.poster_path} 
                                     alt={movie.title} 
-                                    onClick={() => handleOpenMovieUnique(movie.id)}
+                                    onClick={() => handleOpenMovieUnique(movie.id, movie.overview)}
                                 />
                             ) : (
                                 <p>Nous n'avons pas d'image pour ce film</p>
                             )}
-                            <p className='text-dark mt-2'>{movie.title}</p>
-                        </Paper>
+                            <div style={{ fontSize: "14px" }} className='mt-2'>
+                                <p className='m-0 text-dark fw-bold'>{movie.title}</p>
+                                <span>Note moyenne : {movie.vote_average.toFixed(2)} / 10</span><br/>
+                                <span>Date de sortie : {moment(movie.release_date).format('DD/MM/YYYY')}</span>
+                            </div>
+                        </div>
                     )
                 )}
         </div>
--- src/js/pages/uniqueMovie.tsx
+++ src/js/pages/uniqueMovie.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useLocation } from "react-router";
 // AXIOS
 import Axios from "axios";
+// MOMENT
+import moment from "moment";
 // TOKEN
 import token from "../token";
 
 function UniqueMovie() {
-
+    moment.locale("fr");
     const location = useLocation();
     const movie_id = location.state.id;
     const [uniqueData, setUniqueData] = useState({});
-
-    console.log(token);
+    const [genreMovie, setGenreMovie] = useState([]);
+    const [releaseDate, setReleaseDate] = useState("");
 
     const config = {
         params: {
@@ -27,7 +29,22 @@ function UniqueMovie() {
         try {
             const response = await Axios.get('https://api.themoviedb.org/3/movie/' + movie_id, config);
             console.log("Succès : ", response.data);
-            setUniqueData(response.data)
+            setUniqueData(response.data);
+            setGenreMovie(response.data.genres.map((genre) => genre.name));
+        } catch (error) {
+            console.log("Erreur : " + error);
+        }
+    }
+
+    const getDate = async () => {
+        try {
+            const response = await Axios.get(`https://api.themoviedb.org/3/movie/${movie_id}/release_dates`, config)
+            console.log("getDate : ", response.data)
+            response.data.results.map((dates) => {
+                if (dates.iso_3166_1 === "FR") {
+                    setReleaseDate(dates.release_dates.map((date) => moment(date.release_date).format('DD/MM/YYYY')));
+                }
+            })
         } catch (error) {
             console.log("Erreur : " + error);
         }
@@ -35,9 +52,11 @@ function UniqueMovie() {
 
     useEffect(() => {
         getUniqueMovie();
+        getDate();
     }, [movie_id])
 
     return (
+        <>
         <div 
             className="d-flex"
             style={{
@@ -45,9 +64,30 @@ function UniqueMovie() {
                 backgroundSize: "cover"
             }}
         >
-            <img src={'https://media.themoviedb.org/t/p/w220_and_h330_face' + uniqueData.poster_path} />
-            <h3>{uniqueData.title}</h3>
+            <div className="d-flex opacityBackground align-items-center w-100">
+                <div className="d-flex p-5">
+                    <img className="w-100 rounded" src={'https://media.themoviedb.org/t/p/w220_and_h330_face' + uniqueData.poster_path} alt={uniqueData.title} />
+                    <section className="d-flex flex-column justify-content-center p-4 text-white">
+                        <div>
+                            <h3 className="text-white">{uniqueData.title}</h3>
+                            <div>
+                                <span>
+                                    {releaseDate.length >=1 ? releaseDate : 'Pas de date dispo'} (FR)
+                                </span> / 
+                                <span>
+                                    {genreMovie.join()}
+                                </span>
+                            </div>
+                        </div>
+                        <p>TEST</p>
+                        <p>TEST</p>
+                        <p>TEST Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ut magna vel laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis facilisis auctor dui quis placerat. Praesent augue enim, elementum vitae nibh eget, eleifend maximus augue. Maecenas congue nisi non justo mattis, non imperdiet sapien pulvinar. Donec et nisi non enim imperdiet pellentesque eget in tortor. Nulla condimentum aliquet finibus. Nunc id blandit dolor.</p>
+                        <p>TEST</p>
+                    </section>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
 
